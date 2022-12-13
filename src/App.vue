682feb9ac588a71597ea8cc0ba1eca76ed2014ec
weeknd <weeknd.su@gmail.com>
2022-12-13 14:04:33 +0800

    inital

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-app style="font-family: futura">
+    <v-app-bar app color="transparent" flat fixed>
+      <v-btn text class="font-weight-bold">HiGo</v-btn>
+      <v-spacer />
+      <v-btn text>探索·Explore</v-btn>
+      <v-btn text>模版</v-btn>
+      <v-btn text>场景解决方案</v-btn>
+      <v-btn text>体验DEMO</v-btn>
+      <v-spacer />
+      <v-btn text>在线商店</v-btn>
+      <v-btn text>账户</v-btn>
+      <v-btn text @click="right = !right">导航栏</v-btn>
+    </v-app-bar>
+    <v-main class="pa-0">
+      <router-view />
+    </v-main>
+    <v-navigation-drawer right app v-model="right" absolute temporary>
+      <v-toolbar flat>
+        <v-spacer />
+        <v-btn text small color="black" @click="right = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-list nav dense class="mx-2 mt-0">
+        <v-list-item v-for="(item, i) in items" :key="i" link>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"> </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+  data: () => ({
+    right: false,
+    items: [
+      {
+        title: "长期支持",
+      },
+      {
+        title: "Milestone",
+      },
+      {
+        title: "找到我们",
+      },
+      {
+        title: "问题解答",
+      },
+    ],
+  }),
+});
+</script>
+
+<style>
+/**
+隐藏overflow-y轴
+*/
+html {
+  overflow-y: auto;
+}
+
+.lighten-blur {
+  background: rgba(244, 244, 244, 0.65) !important;
+  backdrop-filter: blur(8px) !important;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,53 @@
+<template>
+  <section id="hero" style="background: red">
+    <v-row no-gutters>
+      <v-img src="@/assets/whiteboard.jpg" height="100vh">
+        <v-row class="fill-height ma-0 text-center">
+          <v-col class="align-self-end" cols="12">
+            <v-row class="ma-0" align="center" justify="center">
+              <v-col cols="2">
+                <v-btn rounded class="darken-blur white--text" depressed block>
+                  <v-icon left color="indigo">mdi-heart-cog-outline</v-icon>定制
+                  Higo
+                </v-btn>
+              </v-col>
+              <v-col cols="2">
+                <v-btn rounded class="lighten-blur" depressed block
+                  ><v-icon left color="indigo">mdi-video-outline</v-icon>了解
+                  Higo</v-btn
+                >
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-btn icon class="mb-2">
+                  <v-icon color="black">mdi-arrow-down</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-img>
+    </v-row>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "appMain",
+  data: () => ({}),
+});
+</script>
+
+<style>
+.darken-blur {
+  background: rgba(23, 26, 32, 0.8) !important;
+  backdrop-filter: blur(8px) !important;
+}
+
+.lighten-blur {
+  background: rgba(244, 244, 244, 0.65) !important;
+  backdrop-filter: blur(8px) !important;
+}
+</style>
